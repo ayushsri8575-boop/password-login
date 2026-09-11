df16fd990cdd61c12014e2469ea6ba563c82f93b
ayushsri8575-boop <ayushsri8575@gmail.com>
2026-09-11 15:03:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault(); 
+
+    var usernameInput = document.getElementById("username").value.trim();
+    var passwordInput = document.getElementById("password").value.trim();
+
+    if (usernameInput === "" || passwordInput === "") {
+        alert("pleas input username or password");
+        return;
+    }
+    document.getElementById("loginForm").style.display = "none";
+    document.querySelector(".card h2").style.display = "none";
+    document.getElementById("greetingText").textContent = "Hello, " + usernameInput + "! Hello";
+    document.getElementById("welcomeBox").style.display = "block";
+});
